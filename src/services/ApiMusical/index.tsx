7be2624a45014c4ebe83d9a467c744e5f77866cf
Lucca Zappala <luccazappala@gmail.com>
2025-11-23 16:32:20 -0300

    commit Api do Deezer com gets

--- src/services/ApiMusical/index.tsx
+++ src/services/ApiMusical/index.tsx
@@ -1,12 +1,30 @@
 import React from 'react'
 import { View } from 'react-native'
+import axios, { AxiosResponse } from "axios";
 
-const APIAPI = () => {
-  return (
-    <View>
-      APIAPI      
-    </View>
-  )
-}
+const ApiDeezer = axios.create({
+  baseURL: 'https://api.deezer.com'
+});
 
-export default APIAPI
+export const ApiMusical = {
+  getAlbum: async (albumId: string): Promise<AxiosResponse> => {
+    return ApiDeezer.get(`/album/${albumId}`);  
+  },
+  
+  getArtist: async (artistId: string): Promise<AxiosResponse> => {
+    return ApiDeezer.get(`/artist/${artistId}`);  
+  },
+
+  getTrack: async (trackId: string): Promise<AxiosResponse> => {
+    return ApiDeezer.get(`/track/${trackId}`);  
+  },
+
+  search: async (query: string): Promise<AxiosResponse> => {
+    return ApiDeezer.get(`/search`, {
+      params: { q: query }
+    });  
+  }
+};
+
+export default ApiMusical;
+ 
