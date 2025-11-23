e9640fd98df26b082800456e844c2f5898947536
MarcosFelixmdr <MarcosFelixdev@gmail.com>
2025-11-23 15:53:31 -0300

    ..

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import React from 'react';
 import { Text, TouchableOpacity, View } from 'react-native';
 import { HomePageNavigationProp } from '../../routes/navigators/StackNavigator';
+import { styles } from './styles';
 
 export const Home: React.FC<{ navigation: HomePageNavigationProp }> = ({ navigation }) =>{
   return (
-    <View>
+    <View style={styles.container}>
         
 <Text>Home</Text>
 <TouchableOpacity onPress={() => navigation.navigate('Sobre', {id: 'Grupo4'})}>
--- src/services/ApiMusical/index.tsx
+++ src/services/ApiMusical/index.tsx
@@ -1,8 +1,11 @@
 import React from 'react'
+import { View } from 'react-native'
 
 const APIAPI = () => {
   return (
-    <div>APIAPI</div>
+    <View>
+      APIAPI      
+    </View>
   )
 }
 
