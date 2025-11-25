a038570107871c140885cc419a1defe06709dd47
MarcosFelixmdr <MarcosFelixdev@gmail.com>
2025-11-25 20:31:43 -0300

    merge

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,53 +1,50 @@
 import { Feather } from '@expo/vector-icons';
-import { LinearGradient } from 'expo-linear-gradient';
+import axios from 'axios';
 import React, { useState } from 'react';
 import { Alert, Text, TextInput, TouchableOpacity, View } from 'react-native';
 import { LoginPageNavigationProp } from '../../routes/navigators/StackNavigator';
-import styles from './style';
+import { styles } from './style';
+
+
 export const Login: React.FC<{ navigation: LoginPageNavigationProp }> = ({ navigation }) => {
     const [useremail, setUseremail] = useState('');
     const [password, setPassword] = useState('');
-    const [ registeredUsers, setRegisteredUsers ] = useState( false);
-    const [loading, setLoading] = useState(false);
-    const [error, setError] = useState<string | null>(null);
-    const [userData, setUserData] = useState<any>(null);
-    const apiUrl = 'https://69236cb13ad095fb847084f7.mockapi.io/backstage/usuarios'; 
+    const [usuariosList, setUsuariosList] = useState([]);
+
+    
+    axios.get ('https://69236cb13ad095fb847084f7.mockapi.io/backstage/usuarios',{
+    }).then (response => {
+        setUsuariosList (response.data);
+
+    }); 
     const handleLogin = async () => {
-      console.log('Tentativa de Login:', useremail, password);
-      setLoading(true);
-      setError(null);
-    try {
-        const response = await fetch(apiUrl);
-        const users = await response.json(); 
-        if (!response.ok) {
-             throw new Error('Falha ao buscar usuários na API.');
-        }
-        const userFound = users.find(
-          (user: any) => user.useremail === useremail && user.password === password
+        const usuarioEncontrado = usuariosList.find(
+            (usuario: { useremail: string; password: string }) =>
+                usuario.useremail === useremail && usuario.password === password
         );
-        if (userFound) {
-            console.log('Login bem-sucedido! Dados:', userFound);
-            setUserData(userFound); 
-            Alert.alert('Sucesso', 'Login realizado com êxito!');
+        if (usuarioEncontrado) {
+            Alert.alert('Login bem-sucedido!');
             
         } else {
-            console.error('Falha no Login: Credenciais inválidas.');
-            Alert.alert('Erro', 'Credenciais inválidas. Tente novamente.');
-            setError('Credenciais inválidas.');
+            Alert.alert('Credenciais inválidas. Tente novamente.');
         }
-    } catch (error) {
-        console.error('Erro de rede:', error);
-        Alert.alert('Erro de Conexão', 'Não foi possível conectar ao servidor. Verifique sua MockAPI ou conexão.');
-        setError('Erro de conexão.');
-    } finally {
-        setLoading(false);
-    }
+        try {
+            const response = await axios.get('https://69236cb13ad095fb847084f7.mockapi.io/backstage/usuarios');
+            const usuarios = response.data;
+            setUsuariosList(usuarios);
+        }
+        catch (error) {
+            console.error('Erro ao buscar usuários:', error);
+        }
+    
 };
       return (
         <View style={styles.container}>
+            
             <View style={styles.avatarContainer}>
                 <Feather name="user" size={80} color="#fff" />
             </View>
+
             <TextInput
                 style={styles.input}
                 placeholder="Username"
@@ -56,6 +53,7 @@ export const Login: React.FC<{ navigation: LoginPageNavigationProp }> = ({ navig
                 value={useremail}
                 autoCapitalize="none"
             />
+
             <TextInput
                 style={styles.input}
                 placeholder="Password"
@@ -64,21 +62,20 @@ export const Login: React.FC<{ navigation: LoginPageNavigationProp }> = ({ navig
                 value={password}
                 secureTextEntry 
             />
+
+            <TouchableOpacity >
+            <Text style={styles.linkText}>Esqueceu a Senha?</Text>
+            </TouchableOpacity>            
+
             <TouchableOpacity style={styles.button} onPress={handleLogin}>
                 <Text style={styles.buttonText}>Entrar</Text>
             </TouchableOpacity>
-            <TouchableOpacity >
-            <Text style={styles.linkText}>Esqueceu a Senha?</Text>
-            </TouchableOpacity>
+
             <TouchableOpacity>
             <Text style={styles.linkText}>Criar Conta</Text>
             </TouchableOpacity>
-        <LinearGradient
-                colors={['transparent', '#aa00a9']}
-                start={{ x: 0, y: 1 }}
-                end={{ x: 6, y: 1 }}
-                style={styles.gradient}
-              />
+
+
         </View>
     );
 };
--- src/screens/Player/index.tsx
+++ src/screens/Player/index.tsx
@@ -1,32 +1,25 @@
-import React from 'react';
-import { View, Text, Image, TouchableOpacity } from 'react-native';
 import { useRoute } from '@react-navigation/native';
+import { LinearGradient } from 'expo-linear-gradient';
+import React from 'react';
+import { Image, Text, TouchableOpacity, View } from 'react-native';
 import { styles } from './styles';
 
 export default function PlayerScreen() {
   const route = useRoute();
   const { track } = (route.params || {}) as any;
-
   return (
     <View style={styles.container}>
-     
-      
-    
       <View style={styles.controls}>
         <TouchableOpacity>
           <Text>⟵</Text>
         </TouchableOpacity>
-
         <TouchableOpacity>
           <Text>⏯</Text>
         </TouchableOpacity>
-
         <TouchableOpacity>
           <Text>⟶</Text>
         </TouchableOpacity>
       </View>
-
-      
       <Image
         source={{
           uri:
@@ -36,11 +29,15 @@ export default function PlayerScreen() {
             undefined
         }}
       />
-      
       <Text style={styles.trackTitle}>
         {track?.title} — {track?.artist?.name}
       </Text>
-
+        <LinearGradient
+                colors={['transparent', '#aa00a9']}
+                start={{ x: 0, y: 1 }}
+                end={{ x: 6, y: 1 }}
+                style={styles.gradient}
+              />
     </View>
   );
 }
