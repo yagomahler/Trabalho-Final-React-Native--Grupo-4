a965dfa80a78739dce6143e05d24ec3e81330db6
MarcosFelixmdr <MarcosFelixdev@gmail.com>
2025-11-25 20:11:16 -0300

    Tela de login estilizada

--- src/screens/Login/index.tsx
+++ src/screens/Login/index.tsx
@@ -1,10 +1,9 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, TouchableOpacity, Alert } from 'react-native';
-import styles from './style';
-import { LoginPageNavigationProp } from '../../routes/navigators/StackNavigator';
 import { Feather } from '@expo/vector-icons';
-
-
+import { LinearGradient } from 'expo-linear-gradient';
+import React, { useState } from 'react';
+import { Alert, Text, TextInput, TouchableOpacity, View } from 'react-native';
+import { LoginPageNavigationProp } from '../../routes/navigators/StackNavigator';
+import styles from './style';
 export const Login: React.FC<{ navigation: LoginPageNavigationProp }> = ({ navigation }) => {
     const [useremail, setUseremail] = useState('');
     const [password, setPassword] = useState('');
@@ -12,38 +11,30 @@ export const Login: React.FC<{ navigation: LoginPageNavigationProp }> = ({ navig
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState<string | null>(null);
     const [userData, setUserData] = useState<any>(null);
-    
     const apiUrl = 'https://69236cb13ad095fb847084f7.mockapi.io/backstage/usuarios'; 
     const handleLogin = async () => {
       console.log('Tentativa de Login:', useremail, password);
       setLoading(true);
       setError(null);
-      
-
     try {
         const response = await fetch(apiUrl);
         const users = await response.json(); 
-        
         if (!response.ok) {
              throw new Error('Falha ao buscar usuários na API.');
         }
-
         const userFound = users.find(
           (user: any) => user.useremail === useremail && user.password === password
         );
-        
         if (userFound) {
             console.log('Login bem-sucedido! Dados:', userFound);
             setUserData(userFound); 
             Alert.alert('Sucesso', 'Login realizado com êxito!');
             
         } else {
-        
             console.error('Falha no Login: Credenciais inválidas.');
             Alert.alert('Erro', 'Credenciais inválidas. Tente novamente.');
             setError('Credenciais inválidas.');
         }
-
     } catch (error) {
         console.error('Erro de rede:', error);
         Alert.alert('Erro de Conexão', 'Não foi possível conectar ao servidor. Verifique sua MockAPI ou conexão.');
@@ -54,11 +45,9 @@ export const Login: React.FC<{ navigation: LoginPageNavigationProp }> = ({ navig
 };
       return (
         <View style={styles.container}>
-            
             <View style={styles.avatarContainer}>
                 <Feather name="user" size={80} color="#fff" />
             </View>
-
             <TextInput
                 style={styles.input}
                 placeholder="Username"
@@ -67,7 +56,6 @@ export const Login: React.FC<{ navigation: LoginPageNavigationProp }> = ({ navig
                 value={useremail}
                 autoCapitalize="none"
             />
-
             <TextInput
                 style={styles.input}
                 placeholder="Password"
@@ -76,20 +64,21 @@ export const Login: React.FC<{ navigation: LoginPageNavigationProp }> = ({ navig
                 value={password}
                 secureTextEntry 
             />
-
-            <TouchableOpacity >
-            <Text style={styles.linkText}>Esqueceu a Senha?</Text>
-            </TouchableOpacity>            
-
             <TouchableOpacity style={styles.button} onPress={handleLogin}>
                 <Text style={styles.buttonText}>Entrar</Text>
             </TouchableOpacity>
-
+            <TouchableOpacity >
+            <Text style={styles.linkText}>Esqueceu a Senha?</Text>
+            </TouchableOpacity>
             <TouchableOpacity>
             <Text style={styles.linkText}>Criar Conta</Text>
             </TouchableOpacity>
-
-
+        <LinearGradient
+                colors={['transparent', '#aa00a9']}
+                start={{ x: 0, y: 1 }}
+                end={{ x: 6, y: 1 }}
+                style={styles.gradient}
+              />
         </View>
     );
 };
--- src/screens/Login/style.ts
+++ src/screens/Login/style.ts
@@ -5,7 +5,9 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#1C1C1C",
     alignItems: "center",
-    paddingTop: 80,
+    marginTop: 35,
+    justifyContent: "center",
+    zIndex: 2,
   },
 
   avatarContainer: {
@@ -13,11 +15,13 @@ const styles = StyleSheet.create({
     width: 100,
     height: 100,
     borderRadius: 50,
-    borderWidth: 2,
-    borderColor: "#B0B0B0",
+    borderWidth: 1,
     justifyContent: "center",
     alignItems: "center",
     backgroundColor: "#333333",
+    // borderColor: "#000",
+    boxShadow: "1px 1px 1px 1px #750375ff",
+    zIndex: 2,
   },
 
   input: {
@@ -34,8 +38,10 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.2,
     shadowRadius: 3,
     elevation: 4,
+    borderColor: "#000",
+    // boxShadow: "1px 1px 1px 1px #750375ff",
+    zIndex: 2,
   },
-
   button: {
     width: "85%",
     height: 50,
@@ -45,17 +51,27 @@ const styles = StyleSheet.create({
     alignItems: "center",
     marginTop: 10,
     marginBottom: 10,
+    zIndex: 2,
   },
   buttonText: {
     color: "#1C1C1C",
     fontSize: 18,
     fontWeight: "bold",
+    zIndex: 2,
   },
 
   linkText: {
     color: "#AAAAAA",
     fontSize: 14,
     marginTop: 10,
+    zIndex: 2,
+  },
+  gradient: {
+    position: "absolute",
+    top: 0,
+    right: 0,
+    width: "150%",
+    height: "150%",
   },
 });
 
